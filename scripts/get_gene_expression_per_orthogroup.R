4cf4b2d508797e28a34131b7572fd453470c2556
Alexandre <alexandre.laverre@unil.ch>
2023-07-21 18:02:15 +0200

    add expression score

--- scripts/get_gene_expression_per_orthogroup.R
+++ scripts/get_gene_expression_per_orthogroup.R
@@ -1,49 +1,65 @@
+library(progress)
 path = "/Users/alaverre/Documents/Expressifying/"
 
-tissues <- c("liver", "cerebellum", "kidney", "testis")
+all_orthogroups = read.csv(paste0(path, "data/gene_orthologies/one2one_orthogroups.csv"), row.names = 1)
+tissues <- c("testis", "kidney", "cerebellum")
+min_species=15
 
 for(tissue in tissues){
   print(tissue)
-  gene.expression = readRDS(paste0(path, "data/gene_expression/mammals_", tissue, "_gene_expression.Rds"))
+  
+  gene.expression = readRDS(paste0(path, "data/gene_expression/mammals_", tissue, "_gene_expression_logTPM.Rds"))
+  gene.score = readRDS(paste0(path, "data/gene_expression/mammals_", tissue, "_gene_expression_score.Rds"))
   
   # Correct dog species name 
   dog_index <- which(names(gene.expression) == "Canis_lupus familiaris")
   names(gene.expression)[dog_index] <- "Canis_lupus_familiaris"
+  names(gene.score)[dog_index] <- "Canis_lupus_familiaris"
   
   # Remove the naked-mole rat because OMA annotations (male) are not the same as Bgee (female) 
   gene.expression[["Heterocephalus_glaber"]] <- NULL
+  gene.score[["Heterocephalus_glaber"]] <- NULL
   
   species = names(gene.expression)
+  orthogroups = all_orthogroups[,species]
   
-  orthogroups = read.csv(paste0(path, "data/gene_orthologies/one2one_orthogroups.csv"), row.names = 1)
-  orthogroups = orthogroups[,species]
-  
-  # Select orthogroups with genes for all species
-  #orthogroups_15_sp = orthogroups[rowSums(!is.na(orthogroups)) > 15,]
-  orthogroups_all_sp =  orthogroups[complete.cases(orthogroups),]
+  # Select orthogroups with genes for at least 15 species or all species
+  orthogroups_all_sp = orthogroups[rowSums(!is.na(orthogroups)) >= min_species,]
+  #orthogroups_all_sp =  orthogroups[complete.cases(orthogroups),]
   
   # Create empty data.frame for gene expression in each orthogroup
   samples <- unlist(sapply(gene.expression, function(x) colnames(x)))
   orthogroup_IDs <- row.names(orthogroups_all_sp)
   expression_ortho <- data.frame(matrix(ncol=length(orthogroup_IDs)+1, nrow=length(samples)))
+  
   colnames(expression_ortho) <- c("species", orthogroup_IDs)
   row.names(expression_ortho) <- samples
+  score_ortho <- expression_ortho
   
-  # Complete the data.frame
+  ### Complete the data.frame
+  # Create a progress bar
+  pb <- progress_bar$new(total = length(orthogroup_IDs), format = "[:bar] :percent :elapsed")
+  
   for (orthogroup in orthogroup_IDs){
     for (sp in colnames(orthogroups_all_sp)){
-      
       gene = as.character(orthogroups_all_sp[orthogroup,sp])
       samples = colnames(gene.expression[[sp]])
       
       # Get the expression of this gene if present in data
       if (gene %in% rownames(gene.expression[[sp]])){
         expression = gene.expression[[sp]][gene,]
-      }else{expression = NA}
+        score = gene.score[[sp]][gene,]
+      }else{
+        expression = NA
+        score = NA
+      }
       
       expression_ortho[samples, orthogroup] = expression
       expression_ortho[samples, "species"] = sp
+      score_ortho[samples, orthogroup] = score
+      score_ortho[samples, "species"] = sp
     }
+    pb$tick()
   }
   
   ## Filters to get gene expression for each species in each orthogroup
@@ -52,9 +68,10 @@ for(tissue in tissues){
   
   # Get orthogroups with at least 2 samples for each species
   nb_sp_ortho <- apply(species_without_na[, -1], 2, function(x) sum(x >= 2, na.rm = T))
-  ortho_all_sp <- names(nb_sp_ortho[which(nb_sp_ortho == length(species))])
+  ortho_all_sp <- names(nb_sp_ortho[which(nb_sp_ortho >= min_species)])
   
-  express_ortho_all_sp <- expression_ortho[,c("species", ortho_all_sp)] 
+  express_ortho_all_sp <- expression_ortho[,c("species", ortho_all_sp)]
+  score_ortho_all_sp <- score_ortho[,c("species", ortho_all_sp)] 
   
   # Print summary and save final data.frame per tissue
   print(paste(length(species), "species;", 
@@ -63,21 +80,37 @@ for(tissue in tissues){
   
   # add sample as column
   express_ortho_all_sp <- cbind(sample=row.names(express_ortho_all_sp), express_ortho_all_sp)
-    
-  write.csv(express_ortho_all_sp, file=paste0(path, "results/gene_expression/mammals_", tissue, "_gene_expression_orthogroups.csv"), row.names = F)
+  score_ortho_all_sp <- cbind(sample=row.names(score_ortho_all_sp), score_ortho_all_sp)
+  
+  write.csv(express_ortho_all_sp, file=paste0(path, "results/gene_expression/mammals_", tissue, "_gene_expression_logTPM_orthogroups.csv"), row.names = F)
+  write.csv(score_ortho_all_sp, file=paste0(path, "results/gene_expression/mammals_", tissue, "_gene_expression_score_orthogroups.csv"), row.names = F)
 }
 
+
+
+### All species
 # liver = 24 species, 585 samples, 481 genes.
 # cerebellum = 21 species, 112 samples, 577 genes.
 # kidney = 21 species, 153 samples, 600 genes. 
 # testis = 19 species, 160 samples, 750 genes.
 
-# Example to plot gene expression distribution from a given gene + species
-# in all species where this gene has a 1-to-1 ortholog expressed in the same tissue
+### At least 15 species 
+# liver: 24 species; 585 samples; 8936 genes.
+# cerebellum: 21 species; 112 samples; 7676 genes.
+# kidney: 21 species; 153 samples; 7958 genes.
+# testis: 19 species; 160 samples; 7027 genes.
+
 sp = "Homo_sapiens"
 gene = "ENSG00000109606"
 orthogroup = row.names(orthogroups[which(orthogroups[[sp]] == gene),])
+
+# Directly from orthogroup
 expression = expression_ortho[[orthogroup]]
 
-a <- boxplot(expression~expression_ortho$species, outline=F, 
-        ylab="Gene expression (FPKM)", xlab="Species", main=paste(orthogroup, "in", tissue))
+par(mar = c(7, 4, 2, 2) + 0.1)
+a <- boxplot(expression~expression_ortho$species, outline=F, xaxt = "n",
+             ylab="Gene expression (FPKM)", xlab="", main=paste("DHX15 in", tissue), cex.lab=1)
+
+
+axis(1, at = 1:length(a$names), labels = NA, cex.axis = 1, srt=45, tck = -0.02)
+text(x = 1:length(a$names), y = par("usr")[3] - 3, labels = a$names, srt = 45, adj = 1, xpd = TRUE, cex=0.8)
--- scripts/retrieve_Bgee_data.R
+++ scripts/retrieve_Bgee_data.R
@@ -1,12 +1,171 @@
 library(BgeeDB)
 library(XML)
 
+################################################################################
+formatData <- function (myBgeeObject, data, stats = NULL, callType = "all") 
+{
+  if (length(myBgeeObject$quantitativeData) == 0 | length(myBgeeObject$dataType) == 
+      0) {
+    stop("ERROR: there seems to be a problem with the input Bgee class object, some fields are empty. Please check that the object is valid.")
+  }
+  if (myBgeeObject$quantitativeData != TRUE) {
+    stop("ERROR: formatting quantitative data is not possible for the species and data type of the input Bgee class object.")
+  }
+  if (length(stats) == 0) {
+    if (myBgeeObject$dataType == "affymetrix") {
+      cat("\nWARNING: stats parameter set to \"intensities\" for the next steps.\n")
+      stats <- "intensities"
+    }
+    else if (myBgeeObject$dataType == "rna_seq") {
+      stop("Please specify the stats parameters. Should be set to \"rpkm\", \"counts\" or \"tpm\"")
+    }
+  }
+  else if (myBgeeObject$dataType == "affymetrix" & stats != 
+           "intensities") {
+    cat("\nWARNING: For Affymetrix microarray data, stats parameter can only be set to \"intensities\". This will be used for the next steps.\n")
+    stats <- "intensities"
+  }
+  else if (myBgeeObject$dataType == "rna_seq" & compareVersion(gsub("_", 
+                                                                    ".", myBgeeObject$release), "13.2") <= 0 & !(stats %in% 
+                                                                                                                 c("rpkm", "counts"))) {
+    stop("Choose whether data formatting should create a matrix of RPKMs or read counts, with stats option set as \"rpkm\" or \"counts\"")
+  }
+  else if (myBgeeObject$dataType == "rna_seq" & compareVersion(gsub("_", 
+                                                                    ".", myBgeeObject$release), "13.2") > 0 & !(stats %in% 
+                                                                                                                c("fpkm", "counts", "tpm", "rank"))) {
+    stop("Choose whether data formatting should create a matrix of FPKMs, TPMs or read counts, with stats option set as \"fpkm\", \"tpm\" or \"counts\"")
+  }
+  if (!(callType %in% c("present", "present high quality", 
+                        "all"))) {
+    stop("Choose between displaying intensities for present genes, present high quality genes or all genes, e.g., 'present', 'present high quality', 'all' ")
+  }
+  if (length(data) == 1) 
+    data[[1]]
+  else data
+  cat("\nExtracting expression data matrix...\n")
+  if (stats == "rpkm") {
+    columns <- c("Library.ID", "Gene.ID", "RPKM")
+    expr <- .extract.data(data, columns, callType)
+  }
+  else if (stats == "fpkm") {
+    columns <- c("Library.ID", "Gene.ID", "FPKM")
+    expr <- .extract.data(data, columns, callType)
+  }
+  else if (stats == "tpm") {
+    columns <- c("Library.ID", "Gene.ID", "TPM")
+    expr <- .extract.data(data, columns, callType)
+  }
+  else if (stats == "counts") {
+    columns <- c("Library.ID", "Gene.ID", "Read.count")
+    expr <- .extract.data(data, columns, callType)
+  }
+  else if (stats == "rank") {
+    columns <- c("Library.ID", "Gene.ID", "Rank")
+    expr <- .extract.data(data, columns, callType)
+  }
+  else {
+    columns <- c("Chip.ID", "Probeset.ID", "Log.of.normalized.signal.intensity", 
+                 "Gene.ID")
+    expr <- .extract.data(data, columns, callType)
+  }
+  if (is.data.frame(expr$assayData)) {
+    expr$assayData <- expr$assayData[, order(names(expr$assayData))]
+    expr$pheno <- expr$pheno[order(sampleNames(expr$pheno))]
+    eset <- new("ExpressionSet", exprs = as.matrix(expr$assayData), 
+                phenoData = expr$pheno, featureData = expr$features)
+  }
+  else if (is.list(expr$assayData)) {
+    eset <- mapply(function(x, y, z) {
+      x <- x[, order(names(x))]
+      y <- y[order(sampleNames(y))]
+      new("ExpressionSet", exprs = as.matrix(x), phenoData = y, 
+          featureData = z)
+    }, expr$assayData, expr$pheno, expr$features)
+  }
+  return(eset)
+}
+
+.extract.data <- function (data, columns, callType) 
+{
+  if (class(data) == "list") {
+    calls <- lapply(data, function(x) .calling(x, callType, 
+                                               columns[3]))
+    expr <- lapply(calls, function(x) {
+      xt <- x[, columns[1:3]]
+      xtt <- xt %>% spread_(columns[1], columns[3])
+      rownames(xtt) <- xtt[, columns[2]]
+      xtt[, -1, drop = FALSE]
+    })
+    cat("\nExtracting features information...\n")
+    features <- mapply(.extract.data.feature, calls, expr, 
+                       rep(list(columns), times = length(calls)))
+    cat("\nExtracting samples information...\n")
+    phenos <- mapply(.extract.data.pheno, calls, rep(list(columns[1]), 
+                                                     times = length(calls)))
+  }
+  else {
+    calls <- .calling(data, callType, columns[3])
+    xt <- calls[, columns[1:3]]
+    xtt <- xt %>% spread_(columns[1], columns[3])
+    rownames(xtt) <- xtt[, columns[2]]
+    expr <- xtt[, -1, drop = FALSE]
+    cat("\nExtracting features information...\n")
+    features <- .extract.data.feature(calls, expr, columns)
+    cat("\nExtracting samples information...\n")
+    phenos <- .extract.data.pheno(calls, columns[1])
+  }
+  return(list(assayData = expr, pheno = phenos, features = features, 
+              calls = calls))
+}
+
+.calling <- function (x, callType, column) 
+{
+  if (callType == "present") {
+    cat("  Keeping only present genes.\n")
+    x[(x$Detection.flag == "absent"), column] <- NA
+  }
+  else if (callType == "present high quality") {
+    cat("  Keeping only present high quality genes.\n")
+    x[which(x$Detection.flag == "absent" | x$Detection.quality == 
+              "poor quality"), column] <- NA
+  }
+  return(x)
+}
+
+.extract.data.feature <- function (calls, expr, columns) 
+{
+  if (length(columns) == 3) {
+    fdata <- calls[match(rownames(expr), calls[, columns[2]]), 
+                   columns[2], drop = FALSE]
+  }
+  else if (length(columns) == 4) {
+    fdata <- calls[match(rownames(expr), calls[, columns[2]]), 
+                   columns[c(2, 4)], drop = FALSE]
+  }
+  rownames(fdata) <- fdata[, columns[2]]
+  fdata <- as(fdata, "AnnotatedDataFrame")
+  return(fdata)
+}
+
+.extract.data.pheno <- function (calls, column) 
+{
+  phdata <- calls[, c(column, "Anatomical.entity.ID", "Anatomical.entity.name", 
+                      "Stage.ID", "Stage.name")]
+  phdata <- phdata[!duplicated(phdata[, column]), ]
+  rownames(phdata) <- phdata[, column]
+  phdata <- as.data.frame(phdata)
+  metadata <- data.frame(labelDescription = colnames(phdata), 
+                         row.names = colnames(phdata))
+  phenodata <- new("AnnotatedDataFrame", data = phdata, varMetadata = metadata)
+  return(phenodata)
+}
+################################################################################
+
 path <- "/Users/alaverre/Documents/Expressifying/"
 species <- read.csv(paste0(path, "/data/Bgee_libraries/species_list.csv"), header=T, fill=T)
 mammals <- species[which(species$Large_Taxa == "Mammal" | species$Large_Taxa == "Marsupial" ), "complete_name"]
 #mammals[mammals != "Monodelphis_domestica"]
 
-################################################################################
 # Get numbers of experiments per IDs
 IDs <- list()
 for (sp in mammals){
@@ -44,19 +203,62 @@ names(UBERON) <- names(species_list)
 # Retrieve gene expression data for each tissue
 for (tissu in names(species_list)){
   print(tissu)
-  gene.expression <- list()
-  for (sp in species_list[[tissu]]){
-    print(sp)
-    bgee <- Bgee$new(species=sp, dataType = "rna_seq", pathToData=paste0(path, "/data/Bgee_libraries/"))
-    data <- getData(bgee, anatEntityId = UBERON[[tissu]])
-    simplified.data <- formatData(bgee, data, callType = "present", stats = "fpkm")
-    all_gene_expression <- simplified.data@assayData[["exprs"]]
-    
-    # Remove genes with less than 2 measures
-    gene.expression[[sp]] <- all_gene_expression[rowSums(!is.na(all_gene_expression)) >= 2,]
-    
+  output.expression <- paste0(path, "/data/gene_expression/mammals_", tissu, "_gene_expression_logTPM.Rds")
+  output.score <- paste0(path, "/data/gene_expression/mammals_", tissu, "_gene_expression_score.Rds")
+  
+  if (file.exists(output.expression)){
+    print("Already done!")
+  }else{
+  
+    gene.expression <- list()
+    gene.score <- list()
+    for (sp in species_list[[tissu]]){
+      print(sp)
+      bgee <- Bgee$new(species=sp, dataType = "rna_seq", pathToData=paste0(path, "/data/Bgee_libraries/"))
+      data <- getData(bgee, anatEntityId = UBERON[[tissu]])
+      data$Rank <- as.numeric(data$Rank)
+      simplified.data <- formatData(bgee, data, callType = "present", stats = "tpm")
+      all_gene_expression <- log(simplified.data@assayData[["exprs"]])
+      
+      simplified.data <- formatData(bgee, data, callType = "present", stats = "rank")
+      all_gene_rank <- simplified.data@assayData[["exprs"]]
+      all_gene_rank_normalised <- apply(all_gene_rank, 2, function(x) 100-(x*100)/max(x, na.rm=T))
+      
+      # Remove genes with less than 2 measures
+      gene.expression[[sp]] <- all_gene_expression[rowSums(!is.na(all_gene_expression)) >= 2,]
+      gene.score[[sp]] <- all_gene_rank_normalised[rowSums(!is.na(all_gene_rank_normalised)) >= 2,]
+      
+    }
+    saveRDS(gene.expression, file=output.expression)
+    saveRDS(gene.score, file=output.score)
   }
-  saveRDS(gene.expression, file=paste0(path, "/data/gene_expression/mammals_", tissu, "_gene_expression.Rds"))
 }
+
+sp="Bos_taurus"
+plot(gene.expression[[sp]][,1]~gene.score[[sp]][,1], cex=0.1,
+     xlab="Expression score", ylab="log(TPM)", main=paste(sp, tissu, "in", colnames(gene.score[[sp]])[1]))
+
+par(mfrow=c(2,1))
+par(mai=c(0.4,0.8,0.3,0.3), mgp=c(2.2,0.8,0))
+boxplot(t(gene.expression[[sp]][1:20,]), outline=F, 
+        ylab="log(TPM)", las=1, main="20 genes in liver Bos_taurus")
+boxplot(t(gene.score[[sp]][1:20,]), outline=F,
+        ylab="Score", las=1, main="")
+
+Nsample = length(colnames(gene.expression[[sp]]))
+median_expression <- apply(gene.expression[[sp]][,1:Nsample], 1, function(x) median(log(x), na.rm=T))
+median_score <- apply(gene.score[[sp]][,1:Nsample], 1, function(x) median(x, na.rm=T))
+
+plot(median_expression~median_score, cex=0.1, 
+     xlab="Median score", ylab="median log(TPM)", main=paste("Median across all samples in", tissu, sp))
+
+# SD
+sd_expression <- apply(gene.expression[[sp]][,1:Nsample], 1, function(x) sd(log(x), na.rm=T))
+sd_score <- apply(gene.score[[sp]][,1:Nsample], 1, function(x) sd(x, na.rm=T))
+
+plot(sd_expression~sd_score, cex=0.1, 
+     xlab="Standard Deviation Score", ylab="Standard Deviation log(TPM)", main=paste("SD across all samples in", tissu, sp))
+
+
 
 ################################################################################
